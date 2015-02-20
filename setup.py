019e6f2791bea355c9d37058aa42dab0f88cea89
Alp Aker <alp@food52.com>
2015-02-20 17:13:55 -0500

    Setup file initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(name = 'python_thumbor',
+      version = "1",
+      description = 'Python Thumbor client',
+      author = 'Alp Aker',
+      author_email = 'alp.tekin.aker@gmail.com',
+      zip_safe = False)
