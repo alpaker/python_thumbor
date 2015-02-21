2ca5ca825f404ca1d4a7039090ff33143fe35e9e
Alp Aker <alp@food52.com>
2015-02-21 17:28:54 -0500

    Abstract test framework.

---
+++ test/test_client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import unittest
+import sys
+from os.path import join, abspath, dirname
+
+sys.path.append(abspath(join(dirname(__file__), '..')))
+
+import python_thumbor
+
+class ThumborClientTest(unittest.TestCase):
+
+    pass
