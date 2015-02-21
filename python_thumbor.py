c4db4a77655223b833c07bcd85334cd6e194ff65
Alp Aker <alp@food52.com>
2015-02-20 20:38:29 -0500

    Fix construction of crop spec path component.

--- python_thumbor.py
+++ python_thumbor.py
@@ -42,7 +42,7 @@ class Client(object):
 
         crop_dims = self._get_crop_dims(options)
         if any(x > 0 for x in crop_dims):
-            parts.append("x".join(crop_dims))
+            parts.append("%dx%d:%dx%d" % tuple(crop_dims))
 
         for opt in self.FIT_OPTS:
             if options[opt]:
