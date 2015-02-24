79483d81898d8415b2612e258a04a79e85556f81
Alp Aker <alp@food52.com>
2015-02-24 00:52:34 -0500

    Dynamically generate testcases.

--- test/test_client.py
+++ test/test_client.py
@@ -10,6 +10,7 @@ from python_thumbor import Client, OldClient
 
 KEY = 'my-security-key'
 IMAGE_URI = 'my.domain.com/some/image/url.jpg'
+from data import TEST_DATA
 
 class ClientTest(unittest.TestCase):
 
@@ -31,6 +32,19 @@ class OldClientTest(unittest.TestCase):
     def setUp(self):
         self.client = OldClient(KEY)
 
+def make_fn(opts, expected):
+    def f(self):
+        actual = self.client.uri(**opts)
+        self.assertEquals(expected, actual)
+    return f
+
+for k,v in TEST_DATA.items():
+    name = "test_%s" % k
+    expected_new, expected_old = v['new'], v['old']
+    opts = v['opts'].copy()
+    opts['image'] = IMAGE_URI
+    setattr(ClientTest, name, make_fn(opts, expected_new))
+    setattr(OldClientTest, name, make_fn(opts, expected_old))
 
 if __name__ == "__main__":
 
